660cb1ce17113ee9c2d1b6ddb672b898f93f0096
leebape92 <leebape92@gmail.com>
2025-11-25 21:17:57 +0900

    레스트템플 스프링컨테이너 등록

--- spring-transaction/src/main/java/com/example/demo/config/RestTemplateConfig.java
+++ spring-transaction/src/main/java/com/example/demo/config/RestTemplateConfig.java
@@ -2,11 +2,20 @@ package com.example.demo.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-
+@Configuration
 public class RestTemplateConfig {
 
+	
+	// RestTemplate : Spring에서 외부 API(HTTP 요청)를 보내기 위한 동기(블로킹) 방식의 HTTP 클라이언트
+	
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+	
 	@Bean
 	ApiClient apiClient(RestTemplate restTemplate, @Value("${api.base-url}") String apiBaseUrl, @Value("${api.key}") String apiKey) {
 		return new ApiClient(restTemplate, apiBaseUrl, apiKey);
