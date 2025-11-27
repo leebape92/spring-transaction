54978b7d9eb828c0c603793d2b052284a096ceff
leebape92 <leebape92@gmail.com>
2025-11-27 23:54:17 +0900

    설명 추가

--- spring-transaction/src/main/java/com/example/demo/config/RestTemplateConfig.java
+++ spring-transaction/src/main/java/com/example/demo/config/RestTemplateConfig.java
@@ -10,7 +10,7 @@ public class RestTemplateConfig {
 
 	
 	// RestTemplate : Spring에서 외부 API(HTTP 요청)를 보내기 위한 동기(블로킹) 방식의 HTTP 클라이언트
-	
+	// 동기방식 : HTTP 요청을 보내면 응답이 올 때까지 스레드가 기다리는 구조입니다.
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
