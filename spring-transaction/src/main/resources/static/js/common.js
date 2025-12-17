4a1ca65f4f021ed932195af46b3d0d8148278b2a
leebape92 <leebape92@gmail.com>
2025-12-17 23:39:42 +0900

    들여쓰기 정리

--- spring-transaction/src/main/resources/static/js/common.js
+++ spring-transaction/src/main/resources/static/js/common.js
@@ -1,34 +1,32 @@
 function ajaxPost(url, dto, callback, pAsync) {
-	debugger;
-  $.ajax({
-    url: url,
-    type: "POST",
-    data: JSON.stringify(dto),
-    contentType: "application/json; charset=UTF-8",
-    async: true, // 기본값 비동기 생략가능
+	$.ajax({
+		url: url,
+		type: "POST",
+		data: JSON.stringify(dto),
+		contentType: "application/json; charset=UTF-8",
+		async: true, // 기본값 비동기 생략가능
     success: function(data) {
 		debugger;
-      callback(data); // ✅ 호출부에서 넘긴 콜백 실행
-    },
-    error: function(xhr) {
-	debugger;
-      errMsg = xhr.responseJSON.message;
-      alert(errMsg);
-    }
-  });
+		callback(data); // ✅ 호출부에서 넘긴 콜백 실행
+	},
+	error: function(xhr) {
+		debugger;
+		errMsg = xhr.responseJSON.message;
+		alert(errMsg);
+	}
+	});
 }
 
 function ajaxGet(url, params, callback) {
-	debugger;
 	$.ajax({
-	    url: url,
+		url: url,
 	    type: "GET",
 	    data: params,  // jQuery가 자동으로 쿼리스트링으로 변환해서 요청
 	    success: function(data) {
-	      callback(data);
-    },
+			callback(data);
+	},
     error: function(xhr) {
 		alert(xhr.responseJSON.message);
     }
-  });
+	});
 }
